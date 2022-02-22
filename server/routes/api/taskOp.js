bef4d1fa9b3ef65772cea2309163efd9be9f8446
Kent <leekent173@gmai.com>
2022-02-22 22:59:23 +0800

    modified POST and GET

--- server/routes/api/taskOp.js
+++ server/routes/api/taskOp.js
@@ -2,34 +2,61 @@ const fs = require('fs');
 const path = require('path');
 const taskFilePath = path.resolve(__dirname, "taskdb.json")
 
-function readTaskFile(req, res, next){
+function readTaskFile(){
     console.log("Reading tasks: ", taskFilePath);
     let rawData = fs.readFileSync(taskFilePath);
     console.log(JSON.parse(rawData));
     return JSON.parse(rawData);
 }
 
-function saveNewTask(req, res, next){
+
+// function saveNewTask(req, res, next){
     
-    let newTaskObj = req.body;
-    let tasksArr = JSON.parse(fs.readFileSync(taskFilePath));
-    let taskNameArr = tasksArr.map((task) =>{
-        return task.name;
-    });
+//     let newTaskObj = req.body;
+//     let tasksArr = JSON.parse(fs.readFileSync(taskFilePath));
+//     let taskNameArr = tasksArr.map((task) =>{
+//         return task.name;
+//     });
 
-    if(taskNameArr.includes(newTaskObj.name)){
-        res.send('Task already exist');
-    }else{
-        console.log(`Saving new task:`);
-        console.log(req.body);
+//     if(taskNameArr.includes(newTaskObj.name)){
+//         res.send('Task already exist');
+//     }else{
+//         console.log(`Saving new task:`);
+//         console.log(req.body);
 
-        tasksArr.push(newTaskObj);
-        console.log(tasksArr);
-        console.log(JSON.stringify(tasksArr, null, 2));
-        fs.writeFileSync(taskFilePath, JSON.stringify(tasksArr, null, 2));
-    }
+//         tasksArr.push(newTaskObj);
+//         console.log(tasksArr);
+//         console.log(JSON.stringify(tasksArr, null, 2));
+//         fs.writeFileSync(taskFilePath, JSON.stringify(tasksArr, null, 2));
+//         next();
+//     }
 
-    next();
+async function saveNewTask(task){
+    console.log('Saving Task....')
+    let tasksArr = await readTaskFile();
+    tasksArr.push(task);
+    // console.log(tasksArr);
+    // console.log(JSON.stringify(tasksArr, null, 2));
+    fs.writeFileSync(taskFilePath, JSON.stringify(tasksArr, null, 2));
+ 
 }
 
-module.exports = {readTaskFile, saveNewTask}
+async function taskExist(taskName){
+    let tasksArr = await readTaskFile();
+        let taskNameArr = tasksArr.map((task) =>{
+            return task.name;
+        }); 
+    return taskNameArr.includes(taskName);
+}
+
+async function deleteTask(name){
+    let tasksArr = await readTaskFile();
+    tasksArr.filter((item) => {
+        return item.name !== name;
+    });
+    
+}
+
+    
+
+module.exports = { deleteTask, taskExist ,readTaskFile, saveNewTask}
--- server/routes/api/task_tracker.js
+++ server/routes/api/task_tracker.js
@@ -1,22 +1,34 @@
 const express = require('express');
 const fs = require('fs');
-const {readTaskFile, saveNewTask} = require('./taskOp')
+const { deleteTask ,taskExist, readTaskFile, saveNewTask} = require('./taskOp')
 
 const router = express.Router();
 
 
 
 // get tasks file
-router.get('/', (req,res) => {
-    let taskFile = readTaskFile()
-    res.send(taskFile);
+router.get('/', async (req,res) => {
+    const taskFile = await readTaskFile();
+    res.json(taskFile);
 });
 
 // post added task
-router.post('/',saveNewTask, (req,res) => {
-    res.send();
+router.post('/', async (req,res) => {
+    const newTask = req.body
+    const newTaskExist = await taskExist(newTask.name);
+    if(newTaskExist){
+        res.send('Task Already Exist');
+    }else{
+        await saveNewTask(newTask);
+        res.status(201).send();
+    }
+    
 });
 
 // delete task
+router.delete('/:name', (req, res) =>{
+
+    res.status(200).send();
+})
 
 module.exports = router;
